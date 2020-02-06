0f2e8849a27e235f6a404bc44c720cfebbb5f434
johmedina <32682506+johmedina@users.noreply.github.com>
2020-02-06 15:03:11 +0300

    include node_modules

---
+++ node_modules/react-native-nfc-manager/android/src/main/java/community/revteltech/nfc/TagTechnologyRequest.java
@@ -0,0 +1,109 @@
+package community.revteltech.nfc;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.TagLostException;
+import android.nfc.tech.TagTechnology;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NfcA;
+import android.nfc.tech.NfcB;
+import android.nfc.tech.NfcV;
+import android.nfc.tech.NfcF;
+import android.nfc.tech.MifareClassic;
+import android.nfc.tech.MifareUltralight;
+import android.nfc.tech.IsoDep;
+import android.util.Log;
+import com.facebook.react.bridge.*;
+import java.util.ArrayList;
+
+class TagTechnologyRequest {
+    static String LOG_TAG = "NfcManager-tech";
+    Tag mTag;
+    TagTechnology mTech;
+    String mTechType; // the actual connected type
+    ArrayList<Object> mTechTypes; // the desired types
+    Callback mJsCallback;
+
+    TagTechnologyRequest(ArrayList<Object> techTypes, Callback cb) {
+        mTechTypes = techTypes;
+        mJsCallback = cb;
+    }
+
+    String getTechType() {
+        return mTechType;
+    }
+
+    Callback getPendingCallback() {
+        return mJsCallback;
+    }
+
+    TagTechnology getTechHandle() {
+        return mTech;
+    }
+
+    Tag getTagHandle() {
+        return mTag;
+    }
+
+    boolean isConnected() {
+        return mTech != null;
+    }
+
+    boolean connect(Tag tag) {
+        if (tag == null) {
+            Log.d(LOG_TAG, "received null tag at connect()");
+            return false;
+        }
+
+        for (int i = 0; i < mTechTypes.size(); i++) {
+            String techType = (String)mTechTypes.get(i);
+
+            if (techType.equals("Ndef")) {
+                mTech = Ndef.get(tag);
+            } else if (techType.equals("NfcA")) {
+                mTech = NfcA.get(tag);
+            } else if (techType.equals("NfcB")) {
+                mTech = NfcB.get(tag);
+            } else if (techType.equals("NfcF")) {
+                mTech = NfcF.get(tag);
+            } else if (techType.equals("NfcV")) {
+                mTech = NfcV.get(tag);
+            } else if (techType.equals("IsoDep")) {
+                mTech = IsoDep.get(tag);
+            } else if (techType.equals("MifareClassic")) {
+                mTech = MifareClassic.get(tag);
+            } else if (techType.equals("MifareUltralight")) {
+                mTech = MifareUltralight.get(tag);
+            }
+
+            if (mTech == null) {
+                continue;
+            }
+
+            try {
+                Log.d(LOG_TAG, "connect to " + techType);
+                mTech.connect();
+                mTechType = techType;
+                mTag = tag;
+                return true;
+            } catch (Exception ex) {
+                Log.d(LOG_TAG, "fail to connect tech");
+            }
+        }
+
+        // not connected, restore to default
+        mTech = null;
+        mTechType = null;
+        mTag = null;
+
+        return false;
+    }
+
+    void close() {
+        try {
+            mTech.close();
+        } catch (Exception ex) {
+            Log.d(LOG_TAG, "fail to close tech");
+        }
+    }
+}
---
+++ node_modules/react-native-nfc-manager/ios/NfcManager.h
@@ -0,0 +1,18 @@
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+#elif __has_include("React/RCTBridgeModule.h")
+#import "React/RCTBridgeModule.h"
+#else
+#import "RCTBridgeModule.h"
+#import <React/RCTEventEmitter.h>
+#endif
+#import <CoreNFC/CoreNFC.h>
+
+@interface NfcManager : RCTEventEmitter <RCTBridgeModule, NFCNDEFReaderSessionDelegate, NFCTagReaderSessionDelegate> {
+
+}
+
+@property (strong, nonatomic) NFCNDEFReaderSession *session;
+@property (strong, nonatomic) NFCTagReaderSession *sessionEx;
+@end
